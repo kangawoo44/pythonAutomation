02671a5ab35f0f22db3994acff46b868fbda6240
kangawoo44 <wkang@viv.ai>
2019-02-15 16:05:05 -0800

    change to webbrowser

--- translate/toEnglish.py
+++ translate/toEnglish.py
@@ -13,13 +13,10 @@ else:
     # example text to translate to english: 안녕하세요
     text = pyperclip.paste()
 
-#webbrowser.open('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=' + text)
-res = requests.get('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=' + text)
-try:
-    res.raise_for_status()
-except Exception as exc:
-    print('There was a problem: %s' % (exc))
-
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-#print(soup.prettify())
-#spanElem = soup.select('body > div.frame > div.page.tlid-homepage.homepage.translate-text > div.homepage-content-wrap > div.tlid-source-target.main-header > div.source-target-row > div.tlid-results-container.results-container > div.tlid-result.result-dict-wrapper > div.result.tlid-copy-target > div.text-wrap.tlid-copy-target > div > span.tlid-translation.translation > span')
+webbrowser.open('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=' + text)
+# res = requests.get('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=' + text)
+# try:
+#     res.raise_for_status()
+# except Exception as exc:
+#     print('There was a problem: %s' % (exc))
+# soup = bs4.BeautifulSoup(res.text, 'html.parser')
