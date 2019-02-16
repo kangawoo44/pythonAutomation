4a67dea537f5ab315d91e788367bb5bc4a050603
kangawoo44 <wkang@viv.ai>
2019-02-15 16:53:43 -0800

    checkpoint

--- translate/toEnglish.py
+++ translate/toEnglish.py
@@ -2,7 +2,7 @@
 # toEnglish.py - Launches a google translate in the browser
 # using text from command line or clipboard.
 
-import webbrowser, sys, pyperclip, requests, bs4
+import webbrowser, sys, pyperclip, pyautogui #, requests, bs4
 
 if len(sys.argv) > 1:
     # Get address from command line.
@@ -14,7 +14,8 @@ else:
     # example text to translate to english: 안녕하세요
     text = pyperclip.paste()
 
-webbrowser.open('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=' + text)
+webbrowser.open('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=')
+pyautogui.typewrite(text, 'enter')
 #webbrowser.open('https://translate.google.com/%23view=home&op=translate&sl=auto&tl=en&text=what%20is%20the%20token')
 
 # res = requests.get('https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=' + text)
